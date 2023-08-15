9cdfce1771441af92f2148dfaa1b9641514fd196
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-08-15 11:40:49 +0700

    with auth admin

---
+++ src/lib/withAuthAdmin.js
@@ -0,0 +1,39 @@
+import { withSessionSsr } from "./WithSession";
+
+const WithAuthAdmin = (gssp) =>
+  withSessionSsr(async (context) => {
+    try {
+      const user = context.req.session.user;
+      console.log("masuk", user);
+      if (!user) {
+        return {
+          redirect: {
+            permanent: false,
+            destination: "/authentication/login",
+          },
+        };
+      }
+      if (user.role != "admin") {
+        context.req.session.destroy();
+        return {
+          redirect: {
+            permanent: false,
+            destination: "/absen/login",
+          },
+        };
+      }
+      return await gssp(context);
+    } catch (error) {
+      if (error.response?.data.code === 401) {
+        return {
+          redirect: {
+            permanent: false,
+            destination: "/authentication/login",
+          },
+        };
+      }
+    }
+    return await gssp(context);
+  });
+
+export default WithAuthAdmin;

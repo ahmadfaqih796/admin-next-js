3c1244f73acf9583fac3817a8239d639c7a2adc7
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-08-18 11:20:21 +0700

    login

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,28 +1,25 @@
 import { loginService } from "@/lib/services/login";
-import { withSessionRoute } from "@/lib/sessions/withSession";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const res = await request.json();
   try {
     const response = await loginService(res);
-    //  const { job_level, role_id, is_leader } = response.user;
-    //  console.log("hahahah", response.user);
-
-    //  await response.user.save();
+    const session = request.session;
+    console.log("faqih", session);
     return NextResponse.json({
       success: true,
       message: "Berhasil login",
       data: response.user,
     });
   } catch (error) {
-    console.log("errjjj", error);
+    console.log("error", error.response);
     const e = error.toString();
-    if (error?.response) {
-      return res.status(500).json(error.response.data);
-    }
-    return res.status(400).json({
-      message: e,
+    // if (error?.response) {
+    //   return res.status(500).json(error.response.data);
+    // }
+    return NextResponse.json({
+      message: error,
     });
   }
 }

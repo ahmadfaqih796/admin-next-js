e8c336b241c2918f67c6afdad4f5d8f9e759c677
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-08-18 14:07:19 +0700

    loading

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -19,7 +19,6 @@ const useLogin = () => {
   };
 
   const handleLogin = async (event) => {
-    "use server";
     event.preventDefault();
     handleLoading(true);
     const { target } = event;
@@ -31,45 +30,45 @@ const useLogin = () => {
       strategy: "local",
     };
     console.log("masuk mas", data);
-    // try {
-    //   const { data: res } = await Axios.post("/api/login", data);
-    //   setColor("success");
-    //   setMessage("Berhasil login");
-    //   setOpen(true);
-    //   handleLoading(false);
+    try {
+      const { data: res } = await Axios.post("/api/login", data);
+      setColor("success");
+      setMessage("Berhasil login");
+      setOpen(true);
+      handleLoading(false);
 
-    //   if (res.role === "client" || res.role === "client3") {
-    //     return router.replace("/client/all");
-    //   }
+      if (res.role === "client" || res.role === "client3") {
+        return router.replace("/client/all");
+      }
 
-    //   if (res.isLeader) {
-    //     return router.replace("/leader/all");
-    //   }
+      if (res.isLeader) {
+        return router.replace("/leader/all");
+      }
 
-    //   if (res.role === "admin") {
-    //     return router.replace("/dashboards/dashboard");
-    //   }
+      if (res.role === "admin") {
+        return router.replace("/dashboards/dashboard");
+      }
 
-    //   if (res.role === "superadmin") {
-    //     return router.replace("/superadmin/dashboard");
-    //   }
+      if (res.role === "superadmin") {
+        return router.replace("/superadmin/dashboard");
+      }
 
-    //   // handleLoading(false);
-    //   // setColor("error");
-    //   // setOpen(true);
-    //   // setMessage("Unauthorized");
-    //   // await Axios.post("/api/logout");
-    //   return;
-    // } catch (error) {
-    //   console.log("ddddddddd", error?.response);
-    //   handleLoading(false);
-    //   setColor("error");
-    //   setOpen(true);
-    //   setMessage(
-    //     error?.response?.data?.message ?? "Terjadi kesalahan pada server"
-    //   );
-    //   return;
-    // }
+      // handleLoading(false);
+      // setColor("error");
+      // setOpen(true);
+      // setMessage("Unauthorized");
+      // await Axios.post("/api/logout");
+      return;
+    } catch (error) {
+      console.log("ddddddddd", error?.response);
+      handleLoading(false);
+      setColor("error");
+      setOpen(true);
+      setMessage(
+        error?.response?.data?.message ?? "Terjadi kesalahan pada server"
+      );
+      return;
+    }
   };
 
   return {
